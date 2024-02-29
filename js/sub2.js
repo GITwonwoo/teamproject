6a0d7b684b6ab9a5d789e075bf16b36415320864
GITwonwoo <kaneira2178@naver.com>
2024-02-29 17:52:01 +0900

    2024.02.29
    q

--- js/sub2.js
+++ js/sub2.js
@@ -36,16 +36,16 @@ $(function(){
     
     $(window).scroll(function(){
         let ws=$(this).scrollTop();
-        let s2= $('#section3').offset().top;
+        let s3= $('#section3').offset().top;
 
-        if(ws<=s2-600){
+        if(ws<=s3-600){
             $('#section3 .titlebox').css("transform","translateY(100px)").css("opacity", "0");
         }
-        if(ws>s2-600 && ws<=s2+300){     
+        if(ws>s3-600 && ws<=s3+600){     
             $('#section3 .titlebox').css("transform","translateY(0px)").css("opacity", "1");
                         
         }
-        if(ws>s2+300){
+        if(ws>s3+600){
             $('#section3 .titlebox').css("transform","translateY(100px)").css("opacity", "0");
         }
     });
@@ -55,17 +55,17 @@ $(function(){
     
     $(window).scroll(function(){
         let ws=$(this).scrollTop();
-        let s2= $('.label').offset().top;
+        let label= $('.label').offset().top;
 
-        if(ws<=s2-300){
-            $('.label').css("transform","translateY(100px)").css("opacity", "0");
+        if(ws<=label-800){
+            $('.label').css("transform","translateX(100px)").css("opacity", "0");
         }
-        if(ws>s2-600 && ws<=s2+100){     
-            $('.label').css("transform","translateY(0px)").css("opacity", "1");
+        if(ws>label-800 && ws<=label+180){     
+            $('.label').css("transform","translateX(0px)").css("opacity", "1");
                         
         }
-        if(ws>s2+300){
-            $('.label').css("transform","translateY(100px)").css("opacity", "0");
+        if(ws>label+180){
+            $('.label').css("transform","translateX(100px)").css("opacity", "0");
         }
     });
 });
